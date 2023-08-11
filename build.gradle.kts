13036ac2efd9d4c199b0a1d4fc40f84c5ea7ee54
Iterasys <40407006+Iterasys@users.noreply.github.com>
2023-08-10 22:29:55 -0300

    Final Appium

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,7 +13,9 @@ dependencies {
     testImplementation("org.testng:testng:7.8.0")
     testImplementation("org.hamcrest:hamcrest:2.2")
     implementation("io.appium:java-client:8.5.1")
-
+    implementation("io.cucumber:cucumber-java:7.13.0")
+    implementation("io.cucumber:cucumber-testng:7.13.0")
+    implementation("io.cucumber:cucumber-picocontainer:7.11.2")
 }
 
 tasks.test {
